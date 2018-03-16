9ce7ed5cdb5cfb9e634d67d9deb12f52c2b677c8
Kwan Lowe <kwan.lowe@gmail.com>
2018-03-16 17:11:46 -0400

    Example for kairos API

---
+++ klowe/klltest2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python 
+
+from utility.helpers import getArgs
+from utility.helpers import kairosAuthConfig
+import kairos_face
+
+
+def getImage():
+    import cv2
+
+    cam = cv2.VideoCapture(0)
+
+    cv2.namedWindow("PhotoBooth")
+
+    while True:
+        ret, frame = cam.read()
+        cv2.imshow("PhotoBooth", frame)
+        if not ret:
+            break
+        k = cv2.waitKey(1)
+
+        if k % 256 == 32:
+            # SPACE pressed
+            img_name = "portrait.png"
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            break
+
+    cam.release()
+
+    cv2.destroyAllWindows()
+    return("portrait.png")
+
+options = getArgs()
+conf = kairosAuthConfig(options.credentials, options.profile)
+
+app_id=conf['app_id']
+app_key=conf['app_key']
+
+kairos_face.settings.app_id = app_id
+kairos_face.settings.app_key = app_key
+
+galleries_list = kairos_face.get_galleries_names_list()
+
+image_file=getImage()
+
+# kairos_face.enroll_face(file=image_file, subject_id='subject1', gallery_name='a-gallery')
+
+for item in galleries_list:
+    print(type(item))
+    print(item)
--- klowe/utility/helpers.py
+++ klowe/utility/helpers.py
@@ -1,3 +1,22 @@
+def kairosAuthConfig(configFile, profilename):
+    """ Reads config data from file, returns dictionary """
+    import configparser
+    import sys
+    import os.path
+
+    authConfig={}
+    configFile = os.path.expanduser(configFile)
+    config = configparser.RawConfigParser()
+    try:
+        config.read(configFile)
+
+        authConfig['app_id'] = config.get(profilename, 'app_id')
+        authConfig['app_key'] = config.get(profilename, 'app_key')
+        return authConfig
+
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
 
 
 def readAuthConfig(configFile, profilename):
